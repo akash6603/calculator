10d9dc5dbac07194c3912220fc78cd79150d2990
Shrey <shreyumrigar52@gmail.com>
2021-10-18 17:54:08 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { CalculatorKeysComponent } from './calculator-keys/calculator-keys.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    CalculatorKeysComponent
   ],
   imports: [
     BrowserModule
